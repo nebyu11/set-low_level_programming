430c0ccdf07fbcfb1965782d4e6908e0297bbace
Nebyu <nebyu105@gmail.com>
2026-04-12 12:54:17 -0700

    tasks

---
+++ 0x0B-preprocessor/3-function_like_macro.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+ * ABS - Computes the absolute value of a number x.
+ * @x: The number to compute the absolute value of.
+ */
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+
+#endif
---
+++ 0x0B-preprocessor/4-sum.h
@@ -0,0 +1,11 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * SUM - Computes the sum of two numbers.
+ * @x: The first number.
+ * @y: The second number.
+ */
+#define SUM(x, y) ((x) + (y))
+
+#endif
